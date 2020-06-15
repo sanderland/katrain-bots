ebae786ab658433c17be43ce11020a91c3153a7d
Sander Land <sander.land@cognite.com>
2020-06-15 17:03:59 +0200

    sgf

--- analyze_rank.py
+++ analyze_rank.py
@@ -9,7 +9,10 @@ from katrain.core.engine import KataGoEngine
 def averagemod(data):
     sorteddata = sorted(data)
     lendata = len(data)
-    return sum(sorteddata[int(lendata*.2):int(lendata*.8)+1])/((int(lendata*.8)+1)-int(lendata*.2)) # average without the best and worst 20% of ranks
+    return sum(sorteddata[int(lendata * 0.2) : int(lendata * 0.8) + 1]) / (
+        (int(lendata * 0.8) + 1) - int(lendata * 0.2)
+    )  # average without the best and worst 20% of ranks
+
 
 def calculate_rank_for_player(segment_stats, num_intersec, player):
     non_obvious_moves = [
@@ -20,8 +23,12 @@ def calculate_rank_for_player(segment_stats, num_intersec, player):
     num_legal, rank, value = zip(*non_obvious_moves)
     averagemod_rank = averagemod(rank)
     averagemod_len_legal = averagemod(num_legal)
-    n_moves =  math.floor(0.40220696+averagemod_len_legal/(1.313341*(averagemod_rank+1)-0.088646986)) # the averagemod_rank is the outlier free average of the best move from a selection of n_moves with averagemod_len_legal of total legal moves
-    rank_kyu = (math.log10(n_moves*361/num_intersec)-1.9482)/-0.05737 # using the calibration curve of p:pick:rank
+    n_moves = math.floor(
+        0.40220696 + averagemod_len_legal / (1.313341 * (averagemod_rank + 1) - 0.088646986)
+    )  # the averagemod_rank is the outlier free average of the best move from a selection of n_moves with averagemod_len_legal of total legal moves
+    rank_kyu = (
+        math.log10(n_moves * 361 / num_intersec) - 1.9482
+    ) / -0.05737  # using the calibration curve of p:pick:rank
     return rank_kyu
 
 
@@ -63,7 +70,7 @@ def rank_game(game, len_segment):
 
     ranks = [(1, len(moves), calculate_ranks(policy_stats, num_intersec))]  # entire game
     for segment_mid in range(half_seg, len(moves), half_seg):
-        bounds = (segment_mid - half_seg, min(segment_mid + half_seg,len(moves)))
+        bounds = (segment_mid - half_seg, min(segment_mid + half_seg, len(moves)))
         rank = calculate_ranks(policy_stats[bounds[0] : bounds[1]], num_intersec)
         ranks.append((bounds[0] + 1, bounds[1], rank))
     return ranks
@@ -75,7 +82,9 @@ if __name__ == "__main__":
     e_config["max_visits"] = e_config["fast_visits"] = 1  # since it's just policy anyway
     engine = KataGoEngine(kt, e_config)
 
-    for filename in ['sgf_ogs\katrain_tsstaff (6k) vs katrain-weighted (6k) 2020-06-09 15 52 45_W+72.9.sgf']: #glob.glob("sgf_ogs/*.sgf"):
+    for filename in [
+        "sgf_ogs\katrain_tsstaff (6k) vs katrain-weighted (6k) 2020-06-09 15 52 45_W+72.9.sgf"
+    ]:  # glob.glob("sgf_ogs/*.sgf"):
         game = Game(kt, engine, move_tree=KaTrainSGF.parse_file(filename))
         size = game.board_size
         len_segment = round(50 * size[0] * size[1] / 361)
