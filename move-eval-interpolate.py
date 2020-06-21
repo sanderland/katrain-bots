9d2c23275b6eeaa529c4a6827567e4e655c0b793
bale-go <hofler.lajos@gmail.com>
2020-06-21 16:37:27 +0200

    move-eval-plot

---
+++ move-eval-interpolate.py
@@ -0,0 +1,23 @@
+import numpy as np
+from scipy.interpolate import PchipInterpolator
+import matplotlib.pyplot as plt
+
+testcase = np.loadtxt("test-est.csv", delimiter=",")
+x = testcase[:, 0]
+yB = testcase[:, 1]
+yW = testcase[:, 2]
+xn = np.arange(25, max(testcase[:, 0]))
+fBn = PchipInterpolator(x, yB)
+fWn = PchipInterpolator(x, yW)
+yBn = fBn(xn)
+yWn = fWn(xn)
+
+plt.xlabel("Move #")
+plt.ylabel("kyu rank")
+plt.plot(x, yB, 'ok', label='Black estimated kyu')
+plt.plot(x, yW, 'og', label='White estimated kyu')
+plt.plot(xn, yBn, label='Black estimated kyu interpolated')
+plt.plot(xn, yWn, label='White estimated kyu interpolated')
+
+plt.legend()
+plt.show()
