e668f84a2a6f72c0b61035ea67b140c2ee615e15
bale-go <hofler.lajos@gmail.com>
2020-06-27 22:53:49 +0200

    user_data_based_AI_message_at_the_end_of_sgf

--- analyze_rank.py
+++ analyze_rank.py
@@ -5,6 +5,17 @@ from katrain.core.base_katrain import KaTrainBase
 from katrain.core.game import Game, KaTrainSGF
 from katrain.core.engine import KataGoEngine
 
+def gauss(data):
+    return math.exp(-1 * (data) ** 2)
+
+def median(data):
+    sorteddata = sorted(data)
+    lendata = len(data)
+    index = (lendata - 1) // 2
+    if (lendata % 2):
+        return sorteddata[index]
+    else:
+        return (sorteddata[index] + sorteddata[index + 1])/2.0
 
 def averagemod(data):
     sorteddata = sorted(data)
@@ -15,21 +26,35 @@ def averagemod(data):
 
 
 def calculate_rank_for_player(segment_stats, num_intersec, player):
-    non_obvious_moves = [
-        (nl, r, val)
-        for nl, r, val, pl in segment_stats
-        if val < (0.8 * (1 - (num_intersec - nl) / num_intersec * 0.5)) and pl == player
-    ]
-    num_legal, rank, value = zip(*non_obvious_moves)
-    averagemod_rank = averagemod(rank)
-    averagemod_len_legal = averagemod(num_legal)
-    n_moves = math.floor(
-        0.40220696 + averagemod_len_legal / (1.313341 * (averagemod_rank + 1) - 0.088646986)
-    )  # the averagemod_rank is the outlier free average of the best move from a selection of n_moves with averagemod_len_legal of total legal moves
-    rank_kyu = (
-        math.log10(n_moves * 361 / num_intersec) - 1.9482
-    ) / -0.05737  # using the calibration curve of p:pick:rank
-    return rank_kyu
+        non_obvious_moves = [
+            (nl, r, val)
+            for nl, r, val, pl in segment_stats
+            if nl is not None and val < (0.8 * (1 - (num_intersec - nl) / num_intersec * 0.5)) and pl == player
+        ]
+        num_legal, rank, value = zip(*non_obvious_moves)
+        rank = list(rank)
+        for (i, item) in enumerate(rank):
+            if item > num_legal[i] * 0.09:
+                rank[i] = num_legal[i] * 0.09
+        rank = tuple(rank)
+        averagemod_rank = averagemod(rank)
+        averagemod_len_legal = averagemod(num_legal)
+        norm_avemod_len_legal = averagemod_len_legal / num_intersec
+        if averagemod_rank > 0.1:
+            rank_kyu = (
+                -0.97222
+                * math.log(averagemod_rank)
+                / (0.24634 + averagemod_rank * gauss(3.3208 * (norm_avemod_len_legal)))
+                + 12.703 * (norm_avemod_len_legal)
+                + 11.198 * math.log(averagemod_rank)
+                + 12.28 * gauss(2.379 * (norm_avemod_len_legal))
+                - 16.544
+            )
+        else:
+            rank_kyu = -4
+        if rank_kyu < -4:
+            rank_kyu = -4
+        return rank_kyu  # dan rank
 
 
 def calculate_rank_for_player_alternative_try(segment_stats, num_intersec, player):
@@ -68,11 +93,13 @@ def rank_game(game, len_segment):
     num_intersec = size[0] * size[1]
     half_seg = len_segment // 2
 
-    ranks = [(1, len(moves), calculate_ranks(policy_stats, num_intersec))]  # entire game
+    ranks = [[1, len(moves), {}]]  # entire game
     for segment_mid in range(half_seg, len(moves), half_seg):
         bounds = (segment_mid - half_seg, min(segment_mid + half_seg, len(moves)))
         rank = calculate_ranks(policy_stats[bounds[0] : bounds[1]], num_intersec)
-        ranks.append((bounds[0] + 1, bounds[1], rank))
+        ranks.append([bounds[0] + 1, bounds[1], rank])
+    ranks[0][2]['B'] = median([sl[2]['B'] for sl in ranks[1:]])
+    ranks[0][2]['W'] = median([sl[2]['W'] for sl in ranks[1:]])
     return ranks
 
 
